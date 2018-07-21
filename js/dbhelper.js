5cad860afa19838c6774df1879b3f82f7605364b
Kristof Dombi <dombi.kristof@gmail.com>
2018-07-21 15:13:05 +0200

    feat(fetching): return restaurants from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,29 +7,26 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else {
-        // Oops!. Got an error from server.
-        const error = `Request failed. Returned status of ${xhr.status}`;
-        callback(error, null);
-      }
-    };
-    xhr.send();
+  static fetchRestaurantsFromAPI() {
+    return fetch(DBHelper.DATABASE_URL).then(res =>
+      res.json().then(restaurants => {
+        // TODO IDB
+        return restaurants;
+      })
+    );
+  }
+
+  static async fetchRestaurants(cb) {
+    // TODO return cached results
+    const restaurants = await DBHelper.fetchRestaurantsFromAPI();
+    cb(null, restaurants);
   }
 
   /**
